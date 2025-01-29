b3c9574801078fa25c1eedfc244929366d6f7dde
Merve Cerit <mmervecerit@gmail.com>
2025-01-28 18:32:44 -0800

    Update NeutroFeverGuardDelegate.swift

--- NeutroFeverGuard/NeutroFeverGuardDelegate.swift
+++ NeutroFeverGuard/NeutroFeverGuardDelegate.swift
@@ -84,7 +84,7 @@ class NeutroFeverGuardDelegate: SpeziAppDelegate {
     private var healthKit: HealthKit {
         HealthKit {
             CollectSample(
-                HKQuantityType(.stepCount), //add other things here based on data you will collect through health
+                HKQuantityType(.stepCount), // add other things here based on data you will collect through health
                 deliverySetting: .anchorQuery(.automatic)
             )
         }
