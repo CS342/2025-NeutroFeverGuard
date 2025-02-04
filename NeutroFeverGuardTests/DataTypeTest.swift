c7ab6bdc780168a7ad410a6257f2d4495d29ff4e
Sixian Du <dusixian7@gmail.com>
2025-02-03 20:33:17 -0800

    add copyright

--- NeutroFeverGuardTests/DataTypeTest.swift
+++ NeutroFeverGuardTests/DataTypeTest.swift
@@ -1,8 +1,9 @@
 //
-//  Test.swift
-//  NeutroFeverGuardTests
+// This source file is part of the NeutroFeverGuard based on the Stanford Spezi Template Application project
 //
-//  Created by dusixian on 2025/2/2.
+// SPDX-FileCopyrightText: 2025 Stanford University
+//
+// SPDX-License-Identifier: MIT
 //
 
 import Foundation
