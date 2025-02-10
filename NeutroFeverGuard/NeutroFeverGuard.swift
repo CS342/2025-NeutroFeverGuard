ac9dbaa1663370427ba67b88b9b833a65f1f4497
Priyanka Shrestha <priyankaspace@gmail.com>
2025-02-10 09:50:40 -0800

    Added health visualizations

--- NeutroFeverGuard/NeutroFeverGuard.swift
+++ NeutroFeverGuard/NeutroFeverGuard.swift
@@ -22,7 +22,8 @@ struct NeutroFeverGuard: App {
         WindowGroup {
             ZStack {
                 if completedOnboardingFlow {
-                    HomeView()
+                    //HomeView()
+                    HKVisualizationItem(data: [], xName: "Preview x axis", yName: "Preview y axis", title: "Preview Title")
                 } else {
                     EmptyView()
                 }
--- NeutroFeverGuardTests/NeutroFeverGuardTests.swift
+++ NeutroFeverGuardTests/NeutroFeverGuardTests.swift
@@ -16,3 +16,40 @@ class NeutroFeverGuardTests: XCTestCase {
         XCTAssertEqual(Contacts(presentingAccount: .constant(true)).contacts.count, 1)
     }
 }
+
+class HKVisualizationTests: XCTestCase {
+    @MainActor
+    func testHKDataInitialization() throws {
+        // Test HKData struct initialization
+        let testDate = Date()
+        let hkData = HKData(
+            date: testDate,
+            sumValue: 100.0,
+            avgValue: 50.0,
+            minValue: 10.0,
+            maxValue: 90.0
+        )
+        
+        XCTAssertEqual(hkData.date, testDate)
+        XCTAssertEqual(hkData.sumValue, 100.0)
+        XCTAssertEqual(hkData.avgValue, 50.0)
+        XCTAssertEqual(hkData.minValue, 10.0)
+        XCTAssertEqual(hkData.maxValue, 90.0)
+    }
+    
+    @MainActor
+    func testParseValue() throws {
+        // Test parsing different HealthKit quantity types
+        let healthStore = HKHealthStore()
+        
+        // Heart Rate parsing
+        let heartRateQuantity = HKQuantity(unit: HKUnit(from: "count/min"), doubleValue: 70)
+        let heartRateValue = parseValue(quantity: heartRateQuantity, quantityTypeIDF: .heartRate)
+        XCTAssertEqual(heartRateValue, 70.0)
+        
+        // Oxygen Saturation parsing
+        let oxygenSatQuantity = HKQuantity(unit: .percent(), doubleValue: 0.95)
+        let oxygenSatValue = parseValue(quantity: oxygenSatQuantity, quantityTypeIDF: .oxygenSaturation)
+        XCTAssertEqual(oxygenSatValue, 95.0)
+    }
+}
