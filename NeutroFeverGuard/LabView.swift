cb8118bc7e0ae41471fa3bd5c6d9a31b1bd8cea6
Sixian Du <dusixian7@gmail.com>
2025-02-12 22:09:13 -0800

    add copyright

--- NeutroFeverGuard/LabView.swift
+++ NeutroFeverGuard/LabView.swift
@@ -1,3 +1,11 @@
+//
+// This source file is part of the NeutroFeverGuard based on the Stanford Spezi Template Application project
+//
+// SPDX-FileCopyrightText: 2025 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
 import SpeziAccount
 import SwiftUI
 
