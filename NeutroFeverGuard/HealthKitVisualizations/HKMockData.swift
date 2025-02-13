7d8311b8316dd7dbbfcf9ffbc17a5cd9b008da4e
Priyanka Shrestha <priyankaspace@gmail.com>
2025-02-13 08:50:22 -0800

    Update HKMockData

--- NeutroFeverGuard/HealthKitVisualizations/HKMockData.swift
+++ NeutroFeverGuard/HealthKitVisualizations/HKMockData.swift
@@ -6,27 +6,38 @@
 // SPDX-License-Identifier: MIT
 //
 
+import Charts
+import Foundation
+import HealthKit
+import SwiftUI
+
 private func loadMockData() {
     let today = Date()
     self.heartRateData = (0..<10).map {
-        HKData(date: Calendar.current.date(byAdding: .day, value: -$0, to: today)!,
+        HKData(
+               date: Calendar.current.date(byAdding: .day, value: -$0, to: today) ??  Date(),
                sumValue: Double.random(in: 60...120),
                avgValue: 80,
                minValue: 60,
-               maxValue: 120)
+               maxValue: 120
+        )
     }
     self.basalBodyTemperatureData = (0..<10).map {
-        HKData(date: Calendar.current.date(byAdding: .day, value: -$0, to: today)!,
+        HKData(
+               date: Calendar.current.date(byAdding: .day, value: -$0, to: today) ?? Date(),
                sumValue: Double.random(in: 97...99),
                avgValue: 98.6,
                minValue: 97,
-               maxValue: 99)
+               maxValue: 99
+        )
     }
     self.oxygenSaturationData = (0..<10).map {
-        HKData(date: Calendar.current.date(byAdding: .day, value: -$0, to: today)!,
+        HKData(
+               date: Calendar.current.date(byAdding: .day, value: -$0, to: today) ?? Date(),
                sumValue: Double.random(in: 90...100),
                avgValue: 95,
                minValue: 90,
-               maxValue: 100)
+               maxValue: 100
+        )
     }
 }
