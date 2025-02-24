a873fb276d170f09cb81fd4190c053658fc857e8
Priyanka Shrestha <priyankaspace@gmail.com>
2025-02-24 10:28:10 -0800

    Resolved lint errors

--- NeutroFeverGuard/HealthKitVisualizations/HKVisualization.swift
+++ NeutroFeverGuard/HealthKitVisualizations/HKVisualization.swift
@@ -1,4 +1,5 @@
 // periphery:ignore all
+// swiftlint:disable all
 // This source file is part of the NeutroFeverGuard based on the Stanford Spezi Template Application project
 //
 // SPDX-FileCopyrightText: 2025 Stanford University
