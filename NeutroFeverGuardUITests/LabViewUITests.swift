31a1962de415662b754c520ce55c72398d677b1e
Sixian Du <dusixian7@gmail.com>
2025-02-26 12:26:59 -0800

    fix bug

---
+++ NeutroFeverGuardUITests/LabViewUITests.swift
@@ -0,0 +1,7 @@
+//
+// This source file is part of the NeutroFeverGuard based on the Stanford Spezi Template Application project
+//
+// SPDX-FileCopyrightText: 2025 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
