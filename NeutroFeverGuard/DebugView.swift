390f9c769d40835d3cb8ae9ca49d680bc1824e92
mmervecerit <mmervecerit@gmail.com>
2025-03-07 16:29:54 -0800

    cleaned for periphery errors except the debug view
    want to keep debug view in case we encounter any errors.

--- NeutroFeverGuard/DebugView.swift
+++ NeutroFeverGuard/DebugView.swift
@@ -1,4 +1,4 @@
-//
+// periphery:ignore all
 // This source file is part of the NeutroFeverGuard based on the Stanford Spezi Template Application project
 //
 // SPDX-FileCopyrightText: 2025 Stanford University
