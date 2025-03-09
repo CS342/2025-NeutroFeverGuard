53d5791bd9493a03a2f9305a0e99cd86178c2a06
mmervecerit <mmervecerit@gmail.com>
2025-03-08 19:32:25 -0800

    add healthkitservicetests

---
+++ NeutroFeverGuardTests/HealthkitServiceTests.swift
@@ -0,0 +1,11 @@
+//
+// This source file is part of the NeutroFeverGuard based on the Stanford Spezi Template Application project
+//
+// SPDX-FileCopyrightText: 2025 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+import HealthKit
+@testable import NeutroFeverGuard
+import Testing
