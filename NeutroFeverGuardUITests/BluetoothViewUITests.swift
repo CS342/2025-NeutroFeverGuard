f6949c0873f00e636c19a81f52c72b4234123af4
mmervecerit <mmervecerit@gmail.com>
2025-03-10 22:33:50 -0700

    Update BluetoothViewUITests.swift

--- NeutroFeverGuardUITests/BluetoothViewUITests.swift
+++ NeutroFeverGuardUITests/BluetoothViewUITests.swift
@@ -15,11 +15,11 @@ final class BluetoothViewUITests: XCTestCase {
 
         let app = XCUIApplication()
         app.launchArguments = ["--skipOnboarding"]
-        app.deleteAndLaunch(withSpringboardAppName: "NeutroFeverGuard")
+        app.launch()
     }
     
     @MainActor
-    func testBluetoothOffMessageAppears() throws {
+    func testBluetoothView() throws {
         let app = XCUIApplication()
         XCTAssertTrue(app.wait(for: .runningForeground, timeout: 5))
         XCTAssertTrue(app.tabBars["Tab Bar"].buttons["Connect"].waitForExistence(timeout: 2))
@@ -28,11 +28,24 @@ final class BluetoothViewUITests: XCTestCase {
         let bluetoothWarning = app.staticTexts["Please turn on your Bluetooth and CORE sensor."]
         XCTAssertTrue(bluetoothWarning.exists, "Bluetooth warning should appear when Bluetooth is off")
         
-        let nearbyDevices = app.staticTexts["Nearby Devices"]
+        let nearbyDevices = app.staticTexts["NEARBY DEVICES"]
         XCTAssertTrue(nearbyDevices.exists, "Nearby Devices section should appear in the Bluetooth view.")
         
-        let connectedDevices = app.staticTexts["Connected Devices"]
+        let connectedDevices = app.staticTexts["CONNECTED DEVICES"]
         XCTAssertTrue(connectedDevices.exists, "Connected Devices section should appear in the Bluetooth view.")
+    }
+    
+    @MainActor
+    func testManualDataEntryButton() throws {
+        let app = XCUIApplication()
+        XCTAssertTrue(app.wait(for: .runningForeground, timeout: 5))
+        XCTAssertTrue(app.tabBars["Tab Bar"].buttons["Connect"].waitForExistence(timeout: 2))
+        app.tabBars["Tab Bar"].buttons["Connect"].tap()
         
+        XCTAssertTrue(app.buttons["Enter Data Manually"].waitForExistence(timeout: 5))
+        XCTAssertTrue(app.buttons["Enter Data Manually"].isEnabled)
+        app.buttons["Enter Data Manually"].tap()
+
+        XCTAssertTrue(app.staticTexts["What data would you like to add?"].waitForExistence(timeout: 5))
     }
 }
