1e530433ea050aa47bb607042786d956e60a6150
mmervecerit <mmervecerit@gmail.com>
2025-03-13 11:14:36 -0700

    Update NotificationManager.swift

--- NeutroFeverGuard/NotificationManager.swift
+++ NeutroFeverGuard/NotificationManager.swift
@@ -11,6 +11,8 @@ import UserNotifications
 
 @Observable
 class NotificationManager: Module, NotificationHandler {
+    let notificationIdentifier = "FebrileNeutropeniaAlert"
+    
     @MainActor
     func receiveIncomingNotification(_ notification: UNNotification) async -> UNNotificationPresentationOptions? {
         [.badge, .banner, .list]
@@ -21,8 +23,10 @@ class NotificationManager: Module, NotificationHandler {
         content.title = title
         content.body = body
         content.sound = .default
-
-        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: nil)
+        
+        UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [notificationIdentifier])
+        
+        let request = UNNotificationRequest(identifier: notificationIdentifier, content: content, trigger: nil)
 
         UNUserNotificationCenter.current().add(request) { error in
             if let error = error {
