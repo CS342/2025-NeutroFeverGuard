888dc5faffc04cb8928fe894eca80a2e21d995a8
Merve Cerit <mmervecerit@gmail.com>
2025-03-14 07:24:09 -0700

    Multiple Notifications Bug Fix (#54)
    # Multiple Notifications Bug Fix
    
    ## :recycle: Current situation & Problem
    We were sending multiple notifications when the febrile neutropenia
    condition is met.
    
    ## :gear: Release Notes 
    Now we only check when the added data is a temperature data. I have
    tested on my iphone, and it works!!
    
    
    
    ## :pencil: Code of Conduct & Contributing Guidelines 
    
    By submitting creating this pull request, you agree to follow our [Code
    of
    Conduct](https://github.com/CS342/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/CS342/.github/blob/main/CONTRIBUTING.md):
    - [X] I agree to follow the [Code of
    Conduct](https://github.com/CS342/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/CS342/.github/blob/main/CONTRIBUTING.md).

--- NeutroFeverGuard/NeutroFeverGuardStandard.swift
+++ NeutroFeverGuard/NeutroFeverGuardStandard.swift
@@ -36,13 +36,15 @@ actor NeutroFeverGuardStandard: Standard,
 
     func add(sample: HKSample) async {
         if FeatureFlags.disableFirebase {
-            logger.debug("Received new HealthKit sample: \(sample)")
-            if let condition = await checkForFebrileNeutropenia() {
-                print("Send notification")
-                notificationManager.sendLocalNotification(
-                    title: "Health Alert",
-                    body: "Risk detected: \(condition), please contact your care provider."
-                )
+            logger.info("Received new HealthKit sample: \(sample)")
+            if let quantitySample = sample as? HKQuantitySample,
+                quantitySample.quantityType == HKQuantityType.quantityType(forIdentifier: .bodyTemperature),
+                let condition = await checkForFebrileNeutropenia() {
+                    print("Send notification")
+                    notificationManager.sendLocalNotification(
+                        title: "Health Alert",
+                        body: "Risk detected: \(condition), please contact your care provider."
+                    )
             } else {
                 notificationManager.resetNotificationState() // Reset when condition resolves
             }
@@ -53,8 +55,10 @@ actor NeutroFeverGuardStandard: Standard,
             try await healthKitDocument(id: sample.id)
                 .setData(from: sample.resource)
             // Check if the condition is met before sending a notification
-            if let condition = await checkForFebrileNeutropenia() {
-                print("Send notification")
+            if let quantitySample = sample as? HKQuantitySample,
+                quantitySample.quantityType == HKQuantityType.quantityType(forIdentifier: .bodyTemperature),
+                let condition = await checkForFebrileNeutropenia() {
+                logger.info("Send notification")
                 notificationManager.sendLocalNotification(
                     title: "Health Alert",
                     body: "Risk detected: \(condition), please contact your care provider."
